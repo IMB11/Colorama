d1be7e10f559d5527e62f41e29ab5673fc55d815
Calum <contact@mineblock11.dev>
2024-02-02 18:07:38 +0000

    EnergyBar done!

---
+++ src/Coloring/ComboColorizer.cpp
@@ -0,0 +1,49 @@
+#include <GlobalNamespace/GameEnergyUIPanel.hpp>
+
+#include "Colorama.hpp"
+#include "GlobalNamespace/ComboUIController.hpp"
+#include "HMUI/ImageView.hpp"
+#include "UnityEngine/Time.hpp"
+#include "UnityEngine/Transform.hpp"
+
+using namespace UnityEngine;
+using namespace HMUI;
+using namespace GlobalNamespace;
+
+MAKE_HOOK_MATCH(ComboUIController_StartHook, &ComboUIController::Start, void, ComboUIController* self) {
+  ComboUIController_StartHook(self);
+
+  ComboConfiguration config = getColoramaConfig().comboConfiguration.GetValue();
+  if(self->get_isActiveAndEnabled() && config.enabled) {
+    auto fullComboLines = self->GetComponentsInChildren<ImageView*>();
+
+    if(config.useGradient) {
+      fullComboLines[0]->gradient = true;
+      fullComboLines[0]->set_color(Color::get_white());
+      fullComboLines[1]->gradient = true;
+      fullComboLines[1]->set_color(Color::get_white());
+
+      fullComboLines[0]->color0 = config.topLeft;
+      fullComboLines[0]->color1 = config.topRight;
+
+      if(config.mirrorToBottom) {
+        fullComboLines[1]->color0 = config.topLeft;
+        fullComboLines[1]->color1 = config.topRight;
+      } else {
+        fullComboLines[1]->color0 = config.bottomLeft;
+        fullComboLines[1]->color1 = config.bottomRight;
+      }
+    } else {
+      fullComboLines[0]->gradient = false;
+      fullComboLines[1]->gradient = false;
+      fullComboLines[0]->set_color(config.topLineColor);
+      fullComboLines[1]->set_color(config.bottomLineColor);
+    }
+  }
+}
+
+void ComboColorizerHooks(Logger& logger) {
+  INSTALL_HOOK(logger, ComboUIController_StartHook);
+}
+
+ColoramaInstallHooks(ComboColorizerHooks)
---
+++ src/Coloring/EnergyBarColorizer.cpp
@@ -0,0 +1,119 @@
+#include "Coloring/EnergyBarColorizer.hpp"
+
+#include <UnityEngine/WaitUntil.hpp>
+
+DEFINE_TYPE(Colorama::Coloring, EnergyBarColorizer);
+
+using namespace GlobalNamespace;
+using namespace UnityEngine;
+using namespace Colorama::Coloring;
+
+namespace EnergyBarUtils {
+float repeat(float t, float length = 1.0F) {
+  return std::clamp(t - std::floor(t / length) * length, 0.0f, length);
+}
+
+float pingPong(float t, float length = 1.0F) {
+  t = repeat(t, length * 2.0F);
+  return length - std::abs(t - length);
+}
+}
+
+#include "GlobalNamespace/GameplayModifiers.hpp"
+#include "UnityEngine/Time.hpp"
+#include "System/Func_1.hpp"
+#include "System/Action_1.hpp"
+#include "GlobalNamespace/GameEnergyUIPanel.hpp"
+#include "GlobalNamespace/IGameEnergyCounter.hpp"
+
+void EnergyBarColorizer::Construct(GameEnergyUIPanel* energyPanel) {
+    this->energyPanel = energyPanel;
+}
+
+void EnergyBarColorizer::Update() {
+  if(this->energyPanel == nullptr) return;
+
+  EnergyBarConfiguration config = getColoramaConfig().energyBarConfiguration.GetValue();
+  IGameEnergyCounter* counter = this->energyPanel->gameEnergyCounter;
+  float energy = counter->get_energy();
+
+  if(!config.enabled) return;
+
+  if(config.rainbowFull && (this->energyPanel->gameEnergyCounter->get_instaFail() || (energy == 1.0F && counter->get_energyType() == GameplayModifiers::EnergyType::Bar))) {
+    mainImage->set_color(Color::HSVToRGB(EnergyBarUtils::pingPong(Time::get_time() * 0.5F, 1.0F), 1.0F, 1.0F));
+  }
+}
+
+custom_types::Helpers::Coroutine EnergyBarColorizer::PrepareColors() {
+  std::function predicate_func = [this]() {
+    return this->energyPanel != nullptr;
+  };
+  System::Func_1<bool>* predicate = custom_types::MakeDelegate<System::Func_1<bool>*>(predicate_func);
+  co_yield reinterpret_cast<System::Collections::IEnumerator*>(WaitUntil::New_ctor(predicate));
+
+  EnergyBarConfiguration config = getColoramaConfig().energyBarConfiguration.GetValue();
+  IGameEnergyCounter* counter = this->energyPanel->gameEnergyCounter;
+
+  if(counter->get_energyType() == GameplayModifiers::EnergyType::Battery) {
+    ListW<UnityEngine::UI::Image*> segments = this->energyPanel->batteryLifeSegments;
+    segments[0]->set_color(config.lowColor);
+    segments[1]->set_color(Color::Lerp(config.lowColor, config.midColor, 0.34F));
+    segments[2]->set_color(Color::Lerp(config.midColor, config.highColor, 0.66F));
+    segments[3]->set_color(config.highColor);
+    co_return;
+  }
+
+  mainImage = this->energyPanel->energyBar;
+  if(counter->get_energyType() == GameplayModifiers::EnergyType::Battery) {
+    if(counter->get_instaFail()) {
+      mainImage->set_color(config.highColor);
+    } else {
+      mainImage->set_color(config.midColor);
+    }
+  }
+
+  std::function<void(float)> rawFunc = std::bind(&EnergyBarColorizer::HandleEnergyChange, this, std::placeholders::_1);
+  counter->add_gameEnergyDidChangeEvent(custom_types::MakeDelegate<System::Action_1<float>*>(rawFunc));
+
+  this->HandleEnergyChange(counter->get_energy());
+  
+  co_return;
+}
+
+void EnergyBarColorizer::HandleEnergyChange(float energy) {
+  EnergyBarConfiguration config = getColoramaConfig().energyBarConfiguration.GetValue();
+
+  if(!config.enabled) return;
+
+  if(energy == 0.5F) {
+    mainImage->set_color(config.midColor);
+    return;
+  }
+
+  if(energy > 0.5F) {
+    mainImage->set_color(Color::Lerp(config.midColor, config.highColor, (energy - 0.5f) * 2));
+    return;
+  }
+
+  if(energy < 0.5F) {
+    mainImage->set_color(Color::Lerp(config.lowColor, config.midColor, energy* 2));
+  }
+}
+
+void EnergyBarColorizer::Start() {
+  EnergyBarConfiguration config = getColoramaConfig().energyBarConfiguration.GetValue();
+  if(!config.enabled) return;
+
+  StartCoroutine(custom_types::Helpers::CoroutineHelper::New(PrepareColors()));
+}
+
+MAKE_HOOK_MATCH(GameEnergyUIPanel_InitHook, &GameEnergyUIPanel::Init, void, GameEnergyUIPanel* self) {
+  GameEnergyUIPanel_InitHook(self);
+  self->get_gameObject()->AddComponent<EnergyBarColorizer*>()->Construct(self);
+}
+
+void EnergyBarColorizerHooks(Logger& logger) {
+  INSTALL_HOOK(logger, GameEnergyUIPanel_InitHook);
+}
+
+ColoramaInstallHooks(EnergyBarColorizerHooks)
